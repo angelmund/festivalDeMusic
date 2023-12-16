8a1c5891cb6c7f1fe8d1941c7474c244e1aa0a85
angelmund <97712695+angelmund@users.noreply.github.com>
2023-12-15 19:55:04 -0600

    Cambios en gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const {src,dest, watch} = require("gulp");//es el que está en el package,json
+const {src,dest, watch, parallel} = require("gulp");//es el que está en el package,json
+
+
+//css
 const sass =require("gulp-sass") (require("sass"));
 const plumber = require('gulp-plumber');
 
+//imágenes 
+const webp = require('gulp-webp');
+
+//css
 function css(done){
 
     src('src/scss/**/*.scss') //identificar el archivo de SASS
@@ -12,12 +19,24 @@ function css(done){
     done();
 }
 
+function versionwebp(done){
+
+    const opciones = {
+        quality: 50
+    };    
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css);
     done();
 }
 exports.css = css;
-exports.dev = dev;
+exports.versionwebp = versionwebp;
+exports.dev = parallel( versionwebp, dev);
 
 
 //npx gulp dev 
