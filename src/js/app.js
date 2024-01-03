95d23fdd25b0320365cc73367b593e626664ba9e
angelmund <97712695+angelmund@users.noreply.github.com>
2024-01-02 22:07:32 -0600

    Cambios

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 const { imagenes } = require("../../gulpfile");
+// imagen.index = i;
 
 document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
@@ -11,31 +12,34 @@ function iniciarApp() {
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
-    for (let i = 1; i <= 12; i++) {
+    for (let i=1; i<=12; i++) {
         let imagen = document.createElement('picture');
         //inserta las imagenes en el html
         imagen.innerHTML = `
                 <source srcset="build/img/thumb/${i}.avif" type="imagen/avif">
                 <source srcset="build/img/thumb/${i}.webp" type="imagen/webp">
-                <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen vocalista">
+                <img loading="lazy" with="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen vocalista">
         `;
 
-        imagen.onclick = function () {
-            mostrarImgen(i);
+        // imagen.index = i;
+        
+        imagen.onclick = function() {
+            mostrarImagen(i);
         }
+ 
 
         galeria.appendChild(imagen);
 
     }
 }
 
-function mostrarImgen(id) {
+function mostrarImgen(i) {
     const modalImagen = document.querySelector("picture");
     //abre modal de imagen
     modalImagen.innerHTML = `
-    <source srcset="build/img/grande/${id}.avif" type="imagen/avif">
-    <source srcset="build/img/grande/${id}.webp" type="imagen/webp">
-    <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen vocalista">
+    <source srcset="build/img/grande/${i}.avif" type="imagen/avif">
+    <source srcset="build/img/grande/${i}.webp" type="imagen/webp">
+    <img loading="lazy" with="200" height="300" src="build/img/grande/${i}.jpg" alt="Imagen vocalista">
     `;
 
     const overlay = document.createElement('DIV')
